f7671d6b84c8f9b62160e369eded7e27786a0a62
Sven Kraeuter | 5v3n <sven.kraeuter@gmail.com>
2012-07-18 23:23:47 +0200

    tidied up a little

--- app/models/persistency_manager.rb
+++ app/models/persistency_manager.rb
@@ -3,7 +3,12 @@ require 'redis'
 module SuperUpload
   module PersistencyManager
     def PersistencyManager.redis
-      @redis || @redis = (SuperUpload::REDIS_URI && Redis.new(:host => SuperUpload::REDIS_URI.host, :port => SuperUpload::REDIS_URI.port, :password => SuperUpload::REDIS_URI.password)) || Redis.new
+      @redis || 
+      @redis = (SuperUpload::REDIS_URI && Redis.new(
+          :host => SuperUpload::REDIS_URI.host, 
+          :port => SuperUpload::REDIS_URI.port, 
+          :password => SuperUpload::REDIS_URI.password)) || 
+          Redis.new
     end
     def PersistencyManager.save_hash_value(hash_name, key, value)
       self.redis.hset(hash_name, key, value)
