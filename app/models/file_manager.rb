c5edf21044a34638281bc8fb508133c1ae56a6a3
Sven Kraeuter | 5v3n <sven.kraeuter@gmail.com>
2012-07-19 00:29:18 +0200

    specs for FileManager

--- app/models/file_manager.rb
+++ app/models/file_manager.rb
@@ -5,6 +5,9 @@ module SuperUpload
   class FileManager
     include PersistencyManager
     attr_accessor :upload_progress, :total_size, :path, :sid
+    PATH = "path"
+    UPLOAD_PROGRESS = "upload_progress"
+    TOTAL_SIZE = "total_size"
     def initialize(options = {})
       self.sid              = options[:sid]
       self.upload_progress  = options[:upload_progress]
@@ -12,15 +15,18 @@ module SuperUpload
       self.path             = options[:path]
     end
     def self.find_path(sid)
-      PersistencyManager.find_hash_value "path", sid
+      PersistencyManager.find_hash_value PATH, sid
     end
     def self.find_upload_progress(sid)
-      PersistencyManager.find_hash_value "upload_progress", sid
+      PersistencyManager.find_hash_value UPLOAD_PROGRESS, sid
+    end
+    def self.find_total_size(sid)
+      PersistencyManager.find_hash_value TOTAL_SIZE, sid
     end
     def save
-      PersistencyManager.save_hash_value "upload_progress", @sid, @upload_progress
-      PersistencyManager.save_hash_value "total_size", @sid, @total_size
-      PersistencyManager.save_hash_value "path", @sid, @path
+      PersistencyManager.save_hash_value UPLOAD_PROGRESS, @sid, @upload_progress
+      PersistencyManager.save_hash_value TOTAL_SIZE, @sid, @total_size
+      PersistencyManager.save_hash_value PATH, @sid, @path
     end
   end
 end
--- spec/models/file_manager_spec.rb
+++ spec/models/file_manager_spec.rb
@@ -1,5 +1,50 @@
 require 'spec_helper'
+require './app/models/file_manager.rb'
 
 describe SuperUpload::FileManager do
-  pending "not yet implemented"
+  let(:file_manager){ SuperUpload::FileManager.new(
+    :sid => 'sid', 
+    :upload_progress => 'upload_progress', 
+    :total_size => 'total_size', 
+    :path => 'path' )}
+  it "initializes the parameters given in the options hash" do
+    file_manager
+    file_manager.sid.should             == 'sid'
+    file_manager.upload_progress.should == 'upload_progress'
+    file_manager.total_size.should      == 'total_size'
+    file_manager.path.should            == 'path'
+    file_manager = SuperUpload::FileManager.new(:sid => 'sid')
+    file_manager.sid.should             == 'sid'
+    file_manager.upload_progress.should == nil
+    file_manager.total_size.should      == nil
+    file_manager.path.should            == nil
+  end
+  describe "uses the given persistency and" do
+    let(:sid){ "43243ewsfdf43"}
+    let(:path){ "dfsgfsdg4453/gfbdfg/dgtf.jpg"}
+    let(:upload_progress){ "234"}
+    let(:total_size){ "43543" }
+    before :all do
+      @redis = Redis.new
+    end
+    it "finds all attributes" do
+      @redis.hset SuperUpload::FileManager::PATH, sid, path
+      SuperUpload::FileManager.find_path(sid).should == path
+      @redis.hset SuperUpload::FileManager::UPLOAD_PROGRESS, sid, upload_progress
+      SuperUpload::FileManager.find_upload_progress(sid).should == upload_progress
+      @redis.hset SuperUpload::FileManager::TOTAL_SIZE, sid, total_size
+      SuperUpload::FileManager.find_total_size(sid).should == total_size
+    end
+    it "saves all attributes" do
+      file_manager.save
+      @redis.hget(SuperUpload::FileManager::PATH, sid).should == path
+      @redis.hget(SuperUpload::FileManager::TOTAL_SIZE, sid).should == total_size
+      @redis.hget(SuperUpload::FileManager::UPLOAD_PROGRESS, sid).should == upload_progress
+      SuperUpload::FileManager.new(:sid => 'sid', :path => path).save
+      @redis.hget(SuperUpload::FileManager::PATH, sid).should == path
+      @redis.hget(SuperUpload::FileManager::TOTAL_SIZE, sid).should == nil
+      @redis.hget(SuperUpload::FileManager::UPLOAD_PROGRESS, sid).should == nil
+    end
+  end
+
 end
