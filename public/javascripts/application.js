6c4cde6d4571d0a27904ba0f761e9a8c6832c6d5
Sven Kraeuter | 5v3n <sven.kraeuter@gmail.com>
2012-07-19 09:31:16 +0200

    added uuid-ish id generator

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,17 @@
 window.SuperUpload = window.SuperUpload || {};
 
+//teken from http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
+window.SuperUpload.genereateSid = function(){
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    return v.toString(16);
+  });
+};
+
 $(document).ready(function() {
   var uploadForm = $('.js-upload-form');
   var commentForm = $('.js-comment-form');
-  var sid = new Date().getTime();
+  var sid = window.SuperUpload.genereateSid();
   var interval = null;
 
   commentForm.hide();
