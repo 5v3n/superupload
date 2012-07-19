57094874b8d225917f7e8bc6580efed0b658f247
Sven Kraeuter | 5v3n <sven.kraeuter@gmail.com>
2012-07-19 03:11:35 +0200

    IE treatment...

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,9 +3,22 @@ window.SuperUpload = window.SuperUpload || {};
 $(document).ready(function() {
   var form = $('.js-upload-form');
   var sid = new Date().getTime();
+  var interval = null;
   //submit form when file has been selected
   $('.js-file-upload-input').on('change', function(){
     form.submit();
+    interval = setInterval(function(){
+      $.getJSON('/progress?sid=' + sid, function(data) {
+        var progress = data.progress;
+        if(progress < 100) {
+          $('.js-status').text('Status: ' + progress + '%.');
+        }
+        else{
+          $('.js-status').html('Status: 100%. <a href="'+data.path+'">Find it here.</a>');
+          clearInterval(interval);
+        }
+      })
+    }, 1000);
   });
 
   form.submit(function() {
@@ -14,20 +27,6 @@ $(document).ready(function() {
     //add sid to action
     $(this).attr('action', '/uploads?sid=' + sid);
     $('.js-status').text('starting upload...');
-    window.SuperUpload.updateUploadProgress(sid);
   });
 });
 
-window.SuperUpload.updateUploadProgress = function(sid) {
-    // check upload progess
-    $.get('/progress?sid=' + sid, function(data) {
-      var progress = data.progress || 0
-      if(progress < 100) {
-        if(progress) $('.js-status').text('Status: ' + progress + '%.');
-        setTimeout(function(){window.SuperUpload.updateUploadProgress(sid)},500);
-        return true;
-      }
-      // if progress indicates upload complete, file info
-      $('.js-status').html('Status: 100%. <a href="'+data.path+'">Find it here.</a>');
-    }, 'json');
-  };
