d90c60e0dd2661e522844521a660b2713570c706
Sven Kraeuter | 5v3n <sven.kraeuter@gmail.com>
2012-07-19 12:10:45 +0200

    rewind can be called - it just has no effect anymore

--- lib/monkey_patches.rb
+++ lib/monkey_patches.rb
@@ -6,7 +6,6 @@
 module Unicorn
   class StreamInput
     def rewind
-      puts "******************* Unicorn::StreamInput#rewind called - which should not happen in this app at all. Go check where it's called from and remove it!"
     end
   end
 end
